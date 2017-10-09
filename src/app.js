e4cc3a51604bc89927a9d053d4b193d1f755b572
Madhur <madhur.ahuja@helpchat.in>
2017-10-09 21:53:02 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>An App!</Text>
+      </View>
+    );
+  }
+}
+
