ce4a689cee742c75afe28f336f636440135a1821
Madhur <madhur.ahuja@helpchat.in>
2017-10-10 00:27:17 +0530

    added files

--- src/app.js
+++ src/app.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Text, View } from 'react-native';
+import { Header } from './components/common';
 
 export default class App extends Component {
   render() {
     return (
       <View>
+        <Header headerText="Authentication" />
         <Text>An App!</Text>
       </View>
     );
