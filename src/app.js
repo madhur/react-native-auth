a9ada9cd7d9f13c3fc7d57efb94cdda7bf107418
Madhur <madhur.ahuja@helpchat.in>
2017-10-10 08:06:23 +0530

    firebase setup

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 export default class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBQMr5STMruUGS6W0b93KfhpfUXir1yvSw',
+      authDomain: 'electric-block-696.firebaseapp.com',
+      databaseURL: 'https://electric-block-696.firebaseio.com',
+      projectId: 'electric-block-696',
+      storageBucket: '',
+      messagingSenderId: '128873310281'
+    });
+  }
+
   render() {
     return (
       <View>
